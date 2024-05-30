a687000c07623a6d7e7e13fc3d43c760c20d88ce
yjh0410 <1394571815@qq.com>
2024-05-30 17:38:29 +0800

    fcos-rt & fcos-e2e

--- models/__init__.py
+++ models/__init__.py
@@ -1,39 +1,19 @@
 # Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 import torch
-from .criterion import FcosRTCriterion, FcosE2ECriterion
-from .fcos import FcosRT, FcosE2E
+from .fcos.build     import build_fcos_rt
+from .fcos_e2e.build import build_fcos_e2e
 
 
 def build_model(args, cfg, is_val=False):
-    # ------------ build object detector ------------
-    ## RT-FCOS    
     if   'fcos_rt' in args.model:
-        model = FcosRT(cfg          = cfg,
-                       conf_thresh  = cfg.train_conf_thresh if is_val else cfg.test_conf_thresh,
-                       nms_thresh   = cfg.train_nms_thresh  if is_val else cfg.test_nms_thresh,
-                       topk_results = cfg.train_topk        if is_val else cfg.test_topk,
-                       )
-        criterion = FcosRTCriterion(cfg) if is_val else None
+        model, criterion = build_fcos_rt(args, cfg, is_val)
     elif 'fcos_e2e' in args.model:
-        model = FcosE2E(cfg          = cfg,
-                        conf_thresh  = cfg.train_conf_thresh if is_val else cfg.test_conf_thresh,
-                        topk_results = cfg.train_topk        if is_val else cfg.test_topk,
-                        )
-        criterion = FcosE2ECriterion(cfg) if is_val else None
+        model, criterion = build_fcos_e2e(args, cfg, is_val)
     else:
         raise NotImplementedError("Unknown detector: {}".args.model)
 
-    if is_val:
-        # ------------ Keep training from the given weight ------------
-        if args.resume is not None and args.resume.lower() != "none":
-            print('Load model from the checkpoint: ', args.resume)
-            checkpoint = torch.load(args.resume, map_location='cpu')
-            # checkpoint state dict
-            checkpoint_state_dict = checkpoint.pop("model")
-            model.load_state_dict(checkpoint_state_dict)
-
-        return model, criterion
-
-    else:      
+    if criterion is None:
         return model
+    else:
+        return model, criterion
     
